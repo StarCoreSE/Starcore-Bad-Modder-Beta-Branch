c2b84eb993ac3a66b157005aa81f81d854098e39
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2023-06-23 12:39:14 -0400

    Update VMLS_WeaponPart.cs

--- Data/Scripts/CoreParts/VMLS_WeaponPart.cs
+++ Data/Scripts/CoreParts/VMLS_WeaponPart.cs
@@ -44,7 +44,7 @@ namespace Scripts {
             Targeting = new TargetingDef
             {
                 Threats = new[] {
-                    Grids, // Types of threat to engage: Grids, Projectiles, Characters, Meteors, Neutrals
+                    Grids, Neutrals, // Types of threat to engage: Grids, Projectiles, Characters, Meteors, Neutrals
                 },
                 SubSystems = new[] {
                     Thrust, Utility, Offense, Power, Production, Any, // Subsystem targeting priority: Offense, Utility, Power, Production, Thrust, Jumping, Steering, Any
@@ -86,7 +86,7 @@ namespace Scripts {
             HardPoint = new HardPointDef
             {
                 PartName = "Heavy Bombardment Rockets", // Name of the weapon in terminal, should be unique for each weapon definition that shares a SubtypeId (i.e. multiweapons).
-                DeviateShotAngle = 0.66f, // Projectile inaccuracy in degrees.
+                DeviateShotAngle = 0.80f, // Projectile inaccuracy in degrees.
                 AimingTolerance = 12f, // How many degrees off target a turret can fire at. 0 - 180 firing angle.
                 AimLeadingPrediction = Basic, // Level of turret aim prediction; Off, Basic, Accurate, Advanced
                 DelayCeaseFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 second, etc..). Length of time the weapon continues firing after trigger is released.
